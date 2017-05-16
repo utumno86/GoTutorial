99e85e8997fb95d51be5b9c7c49e1640027f9888
mnash <utumno86@gmail.com>
2017-05-15 23:37:33 -0400

    variadic function

---
+++ variadic/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := average(43, 56, 87, 45, 57)
+	fmt.Println(n)
+}
+
+func average(sf ...float64) float64 {
+
+	fmt.Println(sf)
+	fmt.Printf("%T \n", sf)
+	total := 0.0
+	for _, v := range sf {
+		total += v
+	}
+	return total / float64(len(sf))
+}

d9df407e3473bd6a241b3d3b99fe29d3ee7d96f6
mnash <utumno86@gmail.com>
2017-06-04 15:27:36 -0400

    JSON encoding

---
+++ JSON/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+//Person : A Struct for a person
+type Person struct {
+	First       string
+	Last        string
+	Age         int
+	notExported int
+}
+
+func main() {
+	p1 := Person{"James", "Bond", 20, 007}
+	bs, _ := json.Marshal(p1)
+	fmt.Println(bs)
+	fmt.Printf("%T \n", bs)
+	fmt.Println(string(bs))
+}

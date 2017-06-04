e67ed06e81814b1d99d8727d1fef47c2924a4720
mnash <utumno86@gmail.com>
2017-06-03 21:18:14 -0400

    sherlock holmes

---
+++ sherlockholmes/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Getting Sherlock Holmes this time
+	res, err := http.Get("http://www.gutenberg.org/cache/epub/1661/pg1661.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// scan the page
+	scanner := bufio.NewScanner(res.Body)
+	defer res.Body.Close()
+
+	// Set the split function for the scanning operation
+	scanner.Split(bufio.ScanWords)
+
+	// Create slice of slice of string to hold slices of words
+	buckets := make([][]string, 12)
+
+	// Create slices to hold words words
+	for i := 0; i < 12; i++ {
+		buckets = append(buckets, []string{})
+	}
+
+	// Loop over the words
+	for scanner.Scan() {
+		word := scanner.Text()
+		n := HashBucket(word, 12)
+		buckets[n] = append(buckets[n], word)
+	}
+
+	// Print len of each bucket
+	for i := 0; i < 12; i++ {
+		fmt.Println(i, " - ", len(buckets[i]))
+	}
+}
+
+// HashBucket : Distributes words amongst different buckets
+func HashBucket(word string, buckets int) int {
+	var sum int
+	for _, v := range word {
+		sum += int(v)
+	}
+	return sum % buckets
+}

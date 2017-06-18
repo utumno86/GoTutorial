b21d622ef91b0ab22fc8bf3424f695d42e6b3006
mnash <utumno86@gmail.com>
2017-06-18 12:48:54 -0400

    factorial challenge"

---
+++ factorial/main.go
@@ -0,0 +1,46 @@
+// CHALLENGE #1:
+// -- Use goroutines and channels to calculate 100 factorial computations conncurrently and in parellel
+
+package main
+
+import "fmt"
+
+func main() {
+	in := gen()
+
+	f := factorial(in)
+
+	for n := range f {
+		fmt.Println(n)
+	}
+}
+
+func gen() <-chan int {
+	out := make(chan int)
+	go func() {
+		for i := 1; i < 101; i++ {
+			out <- i
+		}
+		close(out)
+	}()
+	return out
+}
+
+func factorial(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- fact(n)
+		}
+		close(out)
+	}()
+	return out
+}
+
+func fact(n int) int {
+	total := 1
+	for i := n; i > 0; i-- {
+		total *= i
+	}
+	return total
+}
